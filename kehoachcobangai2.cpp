77f99957590d324e0fa077374bc36b1dff15f381
nguyenbalam57 <nguyenbalam57@gmail.com>
2021-03-14 09:33:20 +0700

    file baitap

---
+++ kehoachcobangai2.cpp
@@ -0,0 +1,70 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <cstdlib> 
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+
+#include <errno.h>
+#include <iostream>
+#include <windows.h>
+  # define Cô
+  
+  main ( )
+  {
+   dihoc;
+   scanf("100%", &cacem);
+   if (em= = Xinh)
+     line + +;
+  
+   while (! phan hoi)
+   {
+  
+     printf ("Anh yêu Em");
+  
+   scanf("100%", &phan hoi);
+   }
+  
+   if(phan hoi = = "Lesbian dây")
+     main( ); /* tr? v? và l?p l?i quá trình */
+   else(phan hoi = = "Ði shopping nhé anh")
+    exit(1);
+  
+  
+  else if(phan hoi = = "?... ?")
+  {
+  
+  nguoi yeu = = Xinh;
+  tinh yeu = = (co trai tim *)malloc(ve ngoai quyen ru(nguoi yeu));
+  }
+  
+  di nha hang;
+  
+  nha hang:
+  {
+  di an + +;
+  cuoi dua + +;
+  tra -> tien = nguoi yeu -> tien;
+  tro lai (truong);
+  
+  if (thoi gian = = 2.30)
+  di rap phim;
+  
+  rap phim:
+  {
+  xem + +;
+  if(thoi gian nghi)
+  {
+  nuoc ngot + +;
+  bim bim;
+  }
+  }
+  
+  if(thoi gian = = 6.30)
+  di cong vien;
+  }
+  }
